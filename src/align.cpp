324ba842d4be77cd625f77b478b0937211534471
aleksandrina-streltsova <aleksandrina.strelczova@mail.ru>
2021-10-07 20:40:50 +0300

    Add new metric, save aligned point clouds

--- src/align.cpp
+++ src/align.cpp
@@ -5,6 +5,7 @@
 #include <pcl/point_types.h>
 #include <pcl/point_cloud.h>
 #include <pcl/common/time.h>
+#include <pcl/common/norms.h>
 #include <pcl/console/print.h>
 #include <pcl/features/normal_3d_omp.h>
 #include <pcl/features/fpfh_omp.h>
@@ -14,18 +15,18 @@
 #include <pcl/registration/sample_consensus_prerejective.h>
 #include <pcl/features/moment_of_inertia_estimation.h>
 
-#include "../include/config.h"
-#include "../include/csv_parser.h"
+#include "config.h"
+#include "csv_parser.h"
 
 // Types
 typedef pcl::PointXYZ PointT;
 typedef pcl::PointCloud<pcl::Normal> PointCloudN;
 typedef pcl::PointCloud<PointT> PointCloudT;
 typedef pcl::FPFHSignature33 FeatureT;
-typedef pcl::FPFHEstimation<PointT, pcl::Normal, FeatureT> FeatureEstimationT;
+typedef pcl::FPFHEstimationOMP<PointT, pcl::Normal, FeatureT> FeatureEstimationT;
 typedef pcl::PointCloud<FeatureT> FeatureCloudT;
 
-void printTransformation(Eigen::Matrix4f transformation) {
+void printTransformation(const Eigen::Matrix4f &transformation) {
     pcl::console::print_info("    | %6.3f %6.3f %6.3f | \n", transformation(0, 0), transformation(0, 1),
                              transformation(0, 2));
     pcl::console::print_info("R = | %6.3f %6.3f %6.3f | \n", transformation(1, 0), transformation(1, 1),
@@ -36,6 +37,23 @@ void printTransformation(Eigen::Matrix4f transformation) {
     pcl::console::print_info("t = < %0.3f, %0.3f, %0.3f >\n", transformation(0, 3), transformation(1, 3),
                              transformation(2, 3));
     pcl::console::print_info("\n");
+}
+
+int countCorrectCorrespondences(const Eigen::Matrix4f &transformation_gt,
+                                const PointCloudT::Ptr &src, const PointCloudT::Ptr &tgt,
+                                std::vector<int> inliers, float error_threshold) {
+    PointCloudT::Ptr src_aligned(new PointCloudT);
+    pcl::transformPointCloud(*src, *src_aligned, transformation_gt);
+
+    int correct_correspondences = 0;
+    for (int i = 0; i < inliers.size(); ++i) {
+        int idx = inliers[i];
+        float e = pcl::L1_Norm(src_aligned->points[idx].data, tgt->points[idx].data, 3);
+        if (e < error_threshold) {
+            correct_correspondences++;
+        }
+    }
+    return correct_correspondences;
 }
 
 float getAABBDiagonal(const PointCloudT::Ptr &pcd) {
@@ -53,33 +71,30 @@ float getAABBDiagonal(const PointCloudT::Ptr &pcd) {
     return (max_point - min_point).norm();
 }
 
-Eigen::Matrix4f getTransformation(const std::string& csv_path,
-                                  const std::string& src_filename, const std::string& tgt_filename) {
+Eigen::Matrix4f getTransformation(const std::string &csv_path,
+                                  const std::string &src_filename, const std::string &tgt_filename) {
     std::ifstream file(csv_path);
     Eigen::Matrix4f src_position, tgt_position;
-    
+
     CSVRow row;
     while (file >> row) {
         if (row[0] == src_filename) {
             for (int i = 0; i < 16; ++i) {
                 src_position(i / 4, i % 4) = std::stof(row[i + 1]);
             }
-            std::cout << std::endl;
         } else if (row[0] == tgt_filename) {
             for (int i = 0; i < 16; ++i) {
                 tgt_position(i / 4, i % 4) = std::stof(row[i + 1]);
             }
-            std::cout << std::endl;
         }
     }
-    return tgt_position * src_position.inverse();
-
+    return tgt_position.inverse() * src_position;
 }
 
-void filterReciprocalCorrespondents(PointCloudT::Ptr &src, FeatureCloudT::Ptr &src_features,
-                                    PointCloudT::Ptr &tgt, FeatureCloudT::Ptr &tgt_features) {
+void filterReciprocalCorrespondences(PointCloudT::Ptr &src, FeatureCloudT::Ptr &src_features,
+                                     PointCloudT::Ptr &tgt, FeatureCloudT::Ptr &tgt_features) {
     pcl::registration::CorrespondenceEstimation<FeatureT, FeatureT> est;
-    est.setInputCloud(src_features);
+    est.setInputSource(src_features);
     est.setInputTarget(tgt_features);
     pcl::CorrespondencesPtr reciprocal_correspondences(new pcl::Correspondences);
     est.determineReciprocalCorrespondences(*reciprocal_correspondences);
@@ -105,7 +120,9 @@ void filterReciprocalCorrespondents(PointCloudT::Ptr &src, FeatureCloudT::Ptr &s
 
 int main(int argc, char **argv) {
     // Point clouds
+    PointCloudT::Ptr src_initial(new PointCloudT);
     PointCloudT::Ptr src(new PointCloudT);
+    PointCloudT::Ptr src_aligned_gt(new PointCloudT);
     PointCloudT::Ptr src_aligned(new PointCloudT);
     PointCloudT::Ptr tgt(new PointCloudT);
     FeatureCloudT::Ptr src_features(new FeatureCloudT);
@@ -120,25 +137,27 @@ int main(int argc, char **argv) {
     // Load parameters from config
     YamlConfig config;
     config.init(argv[1]);
+    float voxel_size = config.get<float>("voxel_size", 0.001f);
+    int iteration = config.get<int>("iteration", 1e5);
+
 
     // Load src and tgt
     pcl::console::print_highlight("Loading point clouds...\n");
     std::string src_path = config.get<std::string>("source", "source.ply");
     std::string tgt_path = config.get<std::string>("target", "target.ply");
 
-    if (pcl::io::loadPLYFile<PointT>(src_path, *src) < 0 ||
+    if (pcl::io::loadPLYFile<PointT>(src_path, *src_initial) < 0 ||
         pcl::io::loadPLYFile<PointT>(tgt_path, *tgt) < 0) {
         pcl::console::print_error("Error loading src/tgt file!\n");
         return (1);
     }
-    float leaf = config.get<float>("voxel_size", 0.001f);
 
     // Downsample
     pcl::console::print_highlight("Downsampling...\n");
     pcl::VoxelGrid<PointT> grid;
-    grid.setLeafSize(leaf, leaf, leaf);
-    grid.setInputCloud(src);
-    pcl::console::print_highlight("Point cloud downsampled from %zu...", src->size());
+    grid.setLeafSize(voxel_size, voxel_size, voxel_size);
+    grid.setInputCloud(src_initial);
+    pcl::console::print_highlight("Point cloud downsampled from %zu...", src_initial->size());
     grid.filter(*src);
     pcl::console::print_highlight("to %zu\n", src->size());
     grid.setInputCloud(tgt);
@@ -149,7 +168,7 @@ int main(int argc, char **argv) {
     // Estimate normals for tgt
     pcl::console::print_highlight("Estimating normals...\n");
     PointCloudN::Ptr normals_src(new PointCloudN), normals_tgt(new PointCloudN);
-    pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> normal_est;
+    pcl::NormalEstimationOMP<pcl::PointXYZ, pcl::Normal> normal_est;
     normal_est.setRadiusSearch(config.get<float>("normal_radius", 0.13f) * model_size);
 
     normal_est.setInputCloud(src);
@@ -173,10 +192,15 @@ int main(int argc, char **argv) {
     fest.setInputNormals(normals_tgt);
     fest.compute(*tgt_features);
 
-    filterReciprocalCorrespondents(src, src_features, tgt, tgt_features);
+    if (config.get<bool>("reciprocal", true)) {
+        pcl::console::print_highlight("Filtering features (reciprocal)...\n");
+        filterReciprocalCorrespondences(src, src_features, tgt, tgt_features);
+    }
 
     // Perform alignment
     pcl::console::print_highlight("Starting alignment...\n");
+    std::cout << "    iteration: " << iteration << std::endl;
+    std::cout << "    voxel size: " << voxel_size << std::endl;
     pcl::SampleConsensusPrerejective<PointT, PointT, FeatureT> align;
 
     align.setInputSource(src);
@@ -185,12 +209,12 @@ int main(int argc, char **argv) {
     align.setInputTarget(tgt);
     align.setTargetFeatures(tgt_features);
 
-    align.setMaximumIterations(config.get<int>("iteration", 1e5)); // Number of RANSAC iterations
+    align.setMaximumIterations(iteration); // Number of RANSAC iterations
     align.setNumberOfSamples(config.get<int>("n_samples", 3)); // Number of points to sample for generating/prerejecting a pose
-    align.setCorrespondenceRandomness(1); // Number of nearest features to use
+    align.setCorrespondenceRandomness(config.get<int>("randomness", 1)); // Number of nearest features to use
     align.setSimilarityThreshold(config.get<float>("edge_thr", 0.95)); // Polygonal edge length similarity threshold
-    align.setMaxCorrespondenceDistance(config.get<float>("distance_thr", 1.5) * leaf); // Inlier threshold
-    align.setInlierFraction(0.25f); // Required inlier fraction for accepting a pose hypothesis
+    align.setMaxCorrespondenceDistance(config.get<float>("distance_thr", 1.5) * voxel_size); // Inlier threshold
+    align.setInlierFraction(config.get<float>("inlier_fraction", 0.05)); // Required inlier fraction for accepting a pose hypothesis
     {
         pcl::ScopeTime t("Alignment");
         align.align(*src_aligned);
@@ -199,17 +223,22 @@ int main(int argc, char **argv) {
     std::string csv_path = config.get<std::string>("ground_truth", "");
     std::string src_filename = src_path.substr(src_path.find_last_of("/\\") + 1);
     std::string tgt_filename = tgt_path.substr(tgt_path.find_last_of("/\\") + 1);
+    Eigen::Matrix4f transformation_gt = getTransformation(csv_path, src_filename, tgt_filename);
+
+    pcl::transformPointCloud(*src_initial, *src_aligned, align.getFinalTransformation());
+    pcl::transformPointCloud(*src_initial, *src_aligned_gt, transformation_gt);
+    pcl::io::savePLYFileBinary("source_aligned.ply", *src_aligned);
+    pcl::io::savePLYFileBinary("source_aligned_gt.ply", *src_aligned_gt);
 
     if (align.hasConverged()) {
         // Print results
         printf("\n");
         printTransformation(align.getFinalTransformation());
-        printTransformation(getTransformation(csv_path, src_filename, tgt_filename));
-
-        pcl::console::print_info("fitness: %0.7f\n", (float)align.getInliers().size() / (float)src_features->size());
+        printTransformation(transformation_gt);
+        pcl::console::print_info("fitness: %0.7f\n", (float) align.getInliers().size() / (float) src_features->size());
         pcl::console::print_info("inliers_rmse: %0.7f\n", align.getFitnessScore());
-        pcl::console::print_info("Inliers: %i/%i\n", align.getInliers().size(), src->size());
-        pcl::io::savePLYFileBinary("source_aligned.ply", *src_aligned);
+        pcl::console::print_info("inliers: %i/%i\n", align.getInliers().size(), src->size());
+        pcl::console::print_info("correct inliers: %i/%i\n", countCorrectCorrespondences(transformation_gt, src, tgt, align.getInliers(), 0.5f * voxel_size), src->size());
     } else {
         pcl::console::print_error("Alignment failed!\n");
         return (1);
