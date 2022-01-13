013e6d64ba3fc05df6b513553fd20ac62ed419b6
aleksandrina-streltsova <aleksandrina.strelczova@mail.ru>
2022-01-13 10:42:33 +0300

    FIX: save histograms and descriptors

--- include/feature_analysis.h
+++ include/feature_analysis.h
@@ -7,32 +7,15 @@
 #include "common.h"
 
 template <typename FeatureT>
-void saveHistograms(const typename pcl::PointCloud<FeatureT>::Ptr &features, const std::string &testname, bool is_source) {
+void saveFeatures(const typename pcl::PointCloud<FeatureT>::Ptr &features, const std::string &testname, bool is_source) {
     pcl::console::print_highlight("Saving %s histograms...\n", is_source ? "source" : "target");
     std::string filepath = constructPath(testname,  std::string("histograms_")+ (is_source ? "src" : "tgt"), "csv");
     std::fstream fout(filepath, std::ios_base::out);
     int n = features->size(), m = features->points[0].descriptorSize();
     for (int i = 0; i < n; ++i) {
+        auto *data = reinterpret_cast<const float *> (&features->points[i]);
         for (int j = 0; j < m; ++j) {
-            fout << features->points[i].histogram[j];
-            if (j != m - 1) {
-                fout << ",";
-            }
-        }
-        fout << "\n";
-    }
-    fout.close();
-}
-
-template <typename FeatureT>
-void saveDescriptors(const typename pcl::PointCloud<FeatureT>::Ptr &features, const std::string &testname, bool is_source) {
-    pcl::console::print_highlight("Saving %s descriptors...\n", is_source ? "source" : "target");
-    std::string filepath = constructPath(testname,  std::string("descriptors_")+ (is_source ? "src" : "tgt"), "csv");
-    std::fstream fout(filepath, std::ios_base::out);
-    int n = features->size(), m = features->points[0].descriptorSize();
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < m; ++j) {
-            fout << features->points[i].descriptor[j];
+            fout << data[j];
             if (j != m - 1) {
                 fout << ",";
             }
