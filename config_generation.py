60c08ea46759015c5c45c3f7fb16c2cb050f91e0
aleksandrina-streltsova <aleksandrina.strelczova@mail.ru>
2022-06-15 16:58:59 +0300

    DEBUG: update scripts and notebook

--- config_generation.py
+++ config_generation.py
@@ -2,27 +2,17 @@ import pandas as pd
 import os
 import click
 
-CONFIG_PATH = 'data/kizhi/kizhi_difficult.yaml'
+CONFIG_PATH = 'data/eth_keypoints_bad.yaml'
 OVERLAPPING_PATH = 'data/kizhi/downsampled/overlapping.csv'
 DIFFICULTY_LEVELS_PATH = 'data/test_levels.csv'
-DIRPATH = 'data/kizhi/downsampled_0.02'
+DIRPATHS = ['data/kizhi/normals', 'data/office', 'data/arch', 'data/trees']
 PARAMETERS = '''
-        voxel_size: 0.02
-        downsample: false
-        iteration: 10000000
-        normal_radius_coef: 4
-        feature_radius_coef: 15
-        distance_thr_coef: 1.5
-        edge_thr: 0.95
-        confidence: 0.999
-        n_samples: 3
-        reciprocal: true
-        randomness: 1
-        inlier_fraction: 0.1
-        ground_truth: data/kizhi/ground_truth.csv
-        descriptor: [fpfh, shot, rops]
-        bf: true
-        debug: true
+        voxel_size: 0.04
+        keypoint: iss
+        density: 0.005
+        downsample: true
+        matching: lr
+        metric: correspondences
 '''
 MIN_OVERLAP = 0.2
 
@@ -46,16 +36,24 @@ MIN_OVERLAP = 0.2
 def generate_config(level):
     df = pd.read_csv(DIFFICULTY_LEVELS_PATH)
     df = df[df['level'] == level]
-    with open(CONFIG_PATH, 'w') as file:
+    with open(CONFIG_PATH, 'a') as file:
         file.write('tests:\n')
-        testnames = df['testname'].values
-        for testname in testnames:
-            f1 = testname[:testname.rfind('kig') - 1] + '.ply'
-            f2 = testname[testname.rfind('kig'):] + '.ply'
-            file.write('    - test:')
-            file.write(PARAMETERS)
-            file.write(f'        source: {os.path.join(DIRPATH, f1)}\n')
-            file.write(f'        target: {os.path.join(DIRPATH, f2)}\n')
+        for f1, f2 in df[['source', 'target']].values:
+            for dirpath in DIRPATHS:
+                if os.path.exists(os.path.join(dirpath, f1)):
+                    file.write('    - test:')
+                    file.write(PARAMETERS)
+                    file.write('        ground_truth: ' + os.path.join(dirpath, 'ground_truth.csv') + '\n')
+                    file.write(f'        source: {os.path.join(dirpath, f1)}\n')
+                    file.write(f'        target: {os.path.join(dirpath, f2)}\n')
+    # filenames = list(sorted(filter(lambda s: s.endswith('.ply'), os.listdir(DIRPATH))))
+    # with open(CONFIG_PATH, 'a') as file:
+    #     for i, f1 in enumerate(filenames):
+    #         for f2 in filenames[:i]:
+    #             file.write('    - test:')
+    #             file.write(PARAMETERS)
+    #             file.write(f'        source: {os.path.join(DIRPATH, f1)}\n')
+    #             file.write(f'        target: {os.path.join(DIRPATH, f2)}\n')
 
 
 if __name__ == '__main__':
